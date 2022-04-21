ff4599f47afea2f768ce98db98c18394bf1049d1
DESKTOP-75SH258\rick <ricardo.castell.@gmx.com>
2022-04-21 13:36:53 +0200

    Creamos las especialidades veterinarias y se las añadimos a los veterinarios, ademas se modifica la clase visitas. close #9

--- pet-clinic-data1/src/main/java/guru/rcm/spfw/model/Vet.java
+++ pet-clinic-data1/src/main/java/guru/rcm/spfw/model/Vet.java
@@ -1,5 +1,18 @@
 package guru.rcm.spfw.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
+	private Set<Speciality> speciality;
+
+	public Set<Speciality> getSpeciality() {
+		return speciality;
+	}
+
+	public void setSpeciality(Set<Speciality> speciality) {
+		this.speciality = speciality;
+	}
+	
+	
 }
